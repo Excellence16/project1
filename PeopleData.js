a89b541f2b99d7d2e0b9f6bd5b93efe11c4c45f2
Excellence16 <127138054+Excellence16@users.noreply.github.com>
2023-03-18 22:10:56 +0100

    Add files via upload

---
+++ PeopleData.js
@@ -0,0 +1,30 @@
+export const peopleData = [
+  {
+    id: 1,
+    name: "john doe",
+    age: "40 years",
+    job: "chemist",
+    img: "https://www.latercera.com/resizer/5yZLj73NqU7v2ZEwAFrw6ktAs38=/380x570/filters:focal(798x354:808x344)/cloudfront-us-east-1.images.arcpublishing.com/copesa/IV4VZGDI6RGFND3OKEVZBXXBH4.jpg",
+  },
+  {
+    id: 2,
+    name: "uche Mensah",
+    age: "28 years",
+    job: "architect",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbHU_z0bZ42U0lafAVmKyC3RG1NZDdw4j0tvvqahtBjkhZglg6vBzmINuiztvoU2H8Sn8&usqp=CAU",
+  },
+  {
+    id: 3,
+    name: "kelvin Owen",
+    age: "25 years",
+    job: "engineer",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQVPzh5wsa2qFeqVgoNv75ZlHIdO_r_kltcCro9qcfIA& s",
+  },
+  {
+    id: 4,
+    name: "Andrew peterson",
+    age: "37 yers",
+    job: "backend developer",
+    img: "https://file1.telestar.fr/var/telestar/storage/images/2015/photos/prison-break-michael-scofield-ressuscite-par-wentworth-miller-149068/1066057-1-fre-FR/Prison-break-Michael-Scofield-ressuscite-par-Wentworth-Miller.jpg?alias=original",
+  },
+];
